8142414ce37edb9da26eaaccf8f41d389bfa4337
Omowonuola Jimoh <ojimoh21@live.co.uk>
2020-04-04 17:04:34 +0000

    Done.

--- lib/find_the_minimum_value.rb
+++ lib/find_the_minimum_value.rb
@@ -1,3 +1,16 @@
 def find_min_value(array)
-  # Add your solution here
+array.min  
 end
+
+
+# def find_max_value(array)
+#   count = 0
+#   min_value = 20
+#   while count < array.length do
+#     if min_value > array[count]
+#       min_value = array[count]
+#     end
+#     count += 1
+#   end
+#   min_value
+# end
